4084884431ee356bd2834558aa48f1cf28996d08
BoneGames <benjaminjgbarnes@gmail.com>
2019-02-22 12:09:59 +1100

    Set Up Login Panels

---
+++ Assets/StartPanelConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPanelConfig : MonoBehaviour
+{
+
+    public GameObject LoginCanvas, NewAccountCanvas, NewPasswordCanvas;
+    void Start()
+    {
+        LoginCanvas.SetActive(true);
+        NewAccountCanvas.SetActive(false);
+        NewPasswordCanvas.SetActive(false);
+    }
+}
---
+++ Assets/Submit.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Submit : MonoBehaviour {
+
+    public InputField password, username, newPassword1, newPassword2, newUsername, email;
+    public Text newAccountFeedback, loginFeedback;
+
+    void Start () {
+		
+	}
+
+
+    void Update () {
+		
+	}
+
+    public void SubmitLoginButton()
+    {
+        if (LoginInputCheck())
+        {
+            loginFeedback.text = "Success!";
+        }
+    }
+
+    public void SubmitNewButton()
+    {
+        if(NewInputCheck())
+        {
+            newAccountFeedback.text = "Success!";
+        }
+    }
+
+    bool NewInputCheck()
+    {
+        if(newPassword1.text != "" && newPassword2.text != "" && newUsername.text != "" && email.text != "")
+        {
+            if(!email.text.Contains("@") || !email.text.Contains(".com") || !email.text.Length > 7)
+            {
+                newAccountFeedback.text = "The Passwords Entered Do not Match";
+                return false;
+            }
+            if(newPassword1.text == newPassword2.text)
+            {
+                return true;
+            }
+            else
+            {
+                newAccountFeedback.text = "The Passwords Entered Do not Match";
+                return false;
+            }
+        }
+        else
+        {
+            newAccountFeedback.text = "Make Sure You Have Entered Your Username, Password, AND confirmed our Password";
+            return false;
+        }
+    }
+
+    bool LoginInputCheck()
+    {
+        if (password.text != "" && newUsername.text != "")
+        {
+            return true;
+        }
+        else
+        {
+            loginFeedback.text = "Make Sure You Have Entered Your Username AND Password";
+            return false;
+        }
+    }
+}
+
