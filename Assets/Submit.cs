4a85d0224c735cf7875db9d1bd89cb6379d962b3
BoneGames <44772942+BoneGames@users.noreply.github.com>
2019-03-13 20:36:45 +1100

    fiddling

--- Assets/Submit.cs
+++ Assets/Submit.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Net.Mail;
+using System.Net;
 using UnityEngine.UI;
 using UnityEngine.Networking;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
 
 public class Submit : MonoBehaviour {
 
     public InputField password, username, newPassword1, newPassword2, newUsername, email, forgotPasswordEmail;
-    public Text newAccountFeedback, loginFeedback;
-    string toolTip;
-
+    public Text newAccountFeedback, loginFeedback, recoverPasswordFeedback;
+    public string toolTip;
 
     public void SubmitLoginButton()
     {
@@ -23,9 +26,7 @@ public class Submit : MonoBehaviour {
     {
         if(NewInputCheck())
         {
-            //newAccountFeedback.text = "Success!";
             StartCoroutine(CreateUser(newUsername.text, newPassword1.text, email.text));
-
         }
     }
 
@@ -38,11 +39,11 @@ public class Submit : MonoBehaviour {
     {
         if(newPassword1.text != "" && newPassword2.text != "" && newUsername.text != "" && email.text != "")
         {
-            if(!email.text.Contains("@") || !email.text.Contains(".com") || email.text.Length < 7)
-            {
-                newAccountFeedback.text = "Enter Valid Email Address";
-                return false;
-            }
+            // if(!email.text.Contains("@") || !email.text.Contains(".com") || email.text.Length < 7)
+            // {
+            //     newAccountFeedback.text = "Enter Valid Email Address";
+            //     return false;
+            // }
             if(newPassword1.text != newPassword2.text)
             {
                 newAccountFeedback.text = "Your Passwords Do not Match";
@@ -70,10 +71,11 @@ public class Submit : MonoBehaviour {
         }
     }
 
-    
+
 
     IEnumerator CreateUser(string _username, string _password, string _email)
     {
+        toolTip = "";
         // Link to PHP
         string createUserURL = "http://localhost/squealsystem/InsertUser.php";
 
@@ -85,7 +87,7 @@ public class Submit : MonoBehaviour {
         insertUserForm.AddField("emailPost", _email);
         
         WWW www = new WWW(createUserURL, insertUserForm);
-
+        
         yield return www;
         toolTip = www.text;
         newAccountFeedback.text = toolTip;
@@ -93,6 +95,7 @@ public class Submit : MonoBehaviour {
 
     IEnumerator Login(string _username, string _password)
     {
+        toolTip = "";
         string loginURL = "http://localhost/squealsystem/Login.php";
 
         // Info to send to the POST variables (empty) in PHP InsertUser script
@@ -111,7 +114,57 @@ public class Submit : MonoBehaviour {
 
     IEnumerator RetrievePassword(string _email)
     {
-        string retrievePasswordURL = "http://localhost/squealsystem/RetrievePassword.php";
-        return null;
+        toolTip = "";
+
+        string retrievePasswordURL = "http://localhost/squealsystem/passwordRecovery.php";
+        WWWForm emailForm = new WWWForm();
+        emailForm.AddField("emailPost", _email);
+        WWW www = new WWW(retrievePasswordURL, emailForm);
+        yield return www;
+
+        
+        if(www.text == "email")
+        {
+            SendEmail(_email);
+            toolTip = "An email with password reset instructions has been sent to " + _email;
+        } else {
+            toolTip = "There is no account associated with that email.";
+        }
+        recoverPasswordFeedback.text = toolTip;
+    }
+
+    public void SendEmail(string email)
+    {
+        string code = RandomString();
+
+        MailMessage mail = new MailMessage();
+        mail.To.Add(email);
+        mail.Subject = "SQueaL Games User Reset";
+        mail.Body = "Hello" + username + 
+            "\nReset your Account using this code: " + code;
+
+        SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+        smtpServer.Port = 25;
+
+        smtpServer.Credentials = new System.Net.NetworkCredential
+            ("benjaminjgbarnes@gmail.com", "benoisdabomb1") 
+            as ICredentialsByHost;
+
+        smtpServer.EnableSsl = true;
+
+        ServicePointManager.ServerCertificateValidationCallback
+            = delegate (object s, X509Certificate cert, X509Chain chain
+            , SslPolicyErrors policyErrors)
+            { return true; };
+
+        smtpServer.Send(mail);
+        Debug.Log("Success");
+    }
+    string RandomString()
+    {
+        int seed = Random.Range(0,System.DateTime.Now.Millisecond);
+        int number = seed * System.DateTime.Now.Second;
+        string randomCode = number.ToString();
+        return randomCode;
     }
 }
